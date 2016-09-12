526c7c713a97c86ba920dfd975cd3d6a9839b676
fallen <fengzilijie@gmail.com>
2016-09-12 18:50:35 +0800

    +

---
+++ component/web.URL拦截/WebViewProxy-master/Examples/ExampleApp_iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExampleApp_iOS
+//
+//  Created by Marcus Westin on 6/13/13.
+//  Copyright (c) 2013 WebViewProxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
